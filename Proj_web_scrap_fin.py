ac4290fdb2285aa0f4a17c03431c9d50832821cb
Y. Maricar <maricar.yasmine@gmail.com>
2017-03-01 15:06:33 +0100

    Update Proj_web_scrap_fin.py

--- Proj_web_scrap_fin.py
+++ Proj_web_scrap_fin.py
@@ -116,18 +116,6 @@ Notes = {}
 # #from selenium.webdriver.common.by import By
 # #from selenium.webdriver.support import expected_conditions as EC
 # 
-# ## use firefox to get page with javascript generated content
-# #with closing(Firefox()) as driver:
-# #    driver.get("http://www.att.com/shop/wireless/devices/smartphones.html")
-# #    button = driver.find_element_by_id('deviceShowAllLink')
-# #    button.click()
-# #    # wait for the page to load
-# #    element = WebDriverWait(driver, 10).until(
-# #    EC.invisibility_of_element_located((By.ID, "deviceShowAllLink"))
-# #    )
-# #    # store it to string variable
-# #    page_source = driver.page_source
-# #
 # #soup = BeautifulSoup(page_source)
 # #items = soup.findAll('div', {"class": "list-item"})
 # #print "items count:",len(items)
